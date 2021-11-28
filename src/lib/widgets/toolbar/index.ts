0cc74be15368e499ee31352ca1e4678a7b182ff6
Lev Chelyadinov <leva181777@gmail.com>
2021-11-28 19:22:55 +0300

    Implement the toolbar

---
+++ src/lib/widgets/toolbar/index.ts
@@ -0,0 +1,3 @@
+export { default as Toolbar } from './ui/toolbar.svelte';
+export { default as Tool } from './ui/tool.svelte';
+export { default as Action } from './ui/action.svelte';
---
+++ src/lib/widgets/toolbar/ui/action.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  import type { SvelteComponent } from "svelte";
+
+  export let icon: typeof SvelteComponent;
+  export let actionName: string;
+  export let color: string;
+</script>
+
+<button
+  class="
+    rounded-md
+    w-12 h-12
+    flex justify-center items-center
+    bg-opacity-80 bg-{color}-800 hover:bg-opacity-80 hover:bg-{color}-700
+  "
+  title={actionName}
+  on:click
+>
+  <svelte:component this={icon} class="w-6 h-6 text-white" />
+</button>
