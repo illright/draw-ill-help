fa2ca63d9ed751b942338004625458366592f66b
Lev Chelyadinov <leva181777@gmail.com>
2021-12-10 16:12:09 +0300

    Add support for badges on action buttons

--- src/lib/widgets/toolbar/ui/action.svelte
+++ src/lib/widgets/toolbar/ui/action.svelte
@@ -1,20 +1,29 @@
 <script lang="ts">
-  import type { SvelteComponent } from "svelte";
+  import type { SvelteComponent } from 'svelte';
 
   export let icon: typeof SvelteComponent;
   export let actionName: string;
-  export let color: string;
+  export let disabled: boolean = false;
+  export let badge: string | null = null;
 </script>
 
 <button
   class="
+    relative
     rounded-md
     w-12 h-12
     flex justify-center items-center
-    bg-opacity-80 bg-{color}-800 hover:bg-opacity-80 hover:bg-{color}-700
+    bg-opacity-80 bg-purple-800 hover:bg-opacity-80 hover:bg-purple-700
+    disabled:bg-gray-400 disabled:cursor-not-allowed
   "
   title={actionName}
+  {disabled}
   on:click
 >
   <svelte:component this={icon} class="w-6 h-6 text-white" />
+  {#if badge !== null}
+    <span class="absolute right-0 top-0 px-1 h-4 text-xs rounded-full bg-red-600 text-white">
+      {badge}
+    </span>
+  {/if}
 </button>
