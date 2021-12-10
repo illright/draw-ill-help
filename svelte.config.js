9cd751d7d93f595f0f0b23e51259f4277d3e5858
Lev Chelyadinov <leva181777@gmail.com>
2021-12-10 15:38:00 +0300

    Add the static adapter for exporting

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,9 @@
 import preprocess from 'svelte-preprocess';
 import Icons from 'unplugin-icons/vite';
+import adapterStatic from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
 	preprocess: [
 		preprocess({
 			postcss: true
@@ -13,6 +12,7 @@ const config = {
 
 	kit: {
 		target: 'body',
+		adapter: adapterStatic(),
 		vite: {
 			plugins: [
 				Icons({
