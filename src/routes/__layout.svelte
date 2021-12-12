34bbdeb04e419faa54aac3287166fb6252ce6310
Lev Chelyadinov <leva181777@gmail.com>
2021-12-12 23:35:31 +0300

    Add a favicon and correct the title

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,5 +1,11 @@
-<script>
+<script lang="ts">
   import '../app.css';
+
+  const basePath = vite.define.basePath;
 </script>
+
+<svelte:head>
+  <link rel="icon" href="{basePath}/FlatColorIconsPicture.svg" type="image/svg+xml">
+</svelte:head>
 
 <slot />
