5d45195bc0409b29de5e68c2f65c47e3af56d38c
Lev Chelyadinov <leva181777@gmail.com>
2022-01-10 14:14:07 +0500

    Resolve ESLint-related TODOs

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,8 +1,6 @@
 /// <reference types="@sveltejs/kit" />
 /// <reference types="unplugin-icons/types/svelte" />
 
-// TODO: figure out how to prevent a false positive here
-/* eslint-disable no-unused-vars */
 declare namespace viteDefine {
   interface Vite {
     define: Define;
